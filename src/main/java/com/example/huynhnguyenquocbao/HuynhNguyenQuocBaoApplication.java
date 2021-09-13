916c2c81e4551e86f7d34649a3d4b778d7af415a
biaoHuynh <hnquocbao2>
2021-09-13 19:17:12 +0700

    Initial commit

---
+++ src/main/java/com/example/huynhnguyenquocbao/HuynhNguyenQuocBaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.huynhnguyenquocbao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuynhNguyenQuocBaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuynhNguyenQuocBaoApplication.class, args);
+    }
+
+}
